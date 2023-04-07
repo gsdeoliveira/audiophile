49565fea3c1f91ce702bd7e9e7544b29ea4a91f6
gsdeoliveira <gabriielsousa2002@gmail.com>
2023-04-06 23:00:19 -0300

    header + hero

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}",
+],
+  theme: {
+    fontFamily: {
+      'manrope': ['Manrope', 'sans-serif']
+    },
+    colors: {
+      'primary': {
+        DEFAULT: '#D87D4A',
+        light: '#FBAF85',
+      },
+      'black': '#191919',
+      'white': {
+        DEFAULT: '#FFFFFF',
+        light: 'rgba(255, 255, 255, .5)',
+        extralight: 'rgba(255, 255, 255, .1)',
+      },
+    },
+    extend: {
+      backgroundImage: {
+        'hero': "url('../src/images/image-hero.jpg')",
+      },
+    },
+  },
+  plugins: [],
+}
+
